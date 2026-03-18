ec72e40db32e0f6d3b3fd187230ffcd4239fd503
xyzsimao <simao234430@126.com>
2026-03-18 15:05:46 +0800

    foo: this will fail agagin

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', [
+            "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
+        ]],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never']
+    }
+};
